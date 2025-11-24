035d8fc0fc00c39cfe933bf30a051f99921f439a
Lei <mail.lei.wang@gmail.com>
2025-11-24 08:26:01 +0100

    in progress

--- ansible-examples.sh
+++ ansible-examples.sh
@@ -1,7 +1,12 @@
 ansible-config list # List all configurations
 ansible-config view # Shows the current config file
 ansible-config dump # Shows the current settings
-
 export ANSIBLE_GATHERING=explicit
 ansible-config dump | grep GATHERING
 # output: DEFAULT_GATHERING(env: ANSIBLE_GATHERING) = explicit
+
+ansible-playbook ./playbook.yml --syntax-check
+ansible-playbook ./playbook.yml --check
+ansible-playbook ./playbook.yml --check --diff
+
+ansible-lint ./playbook.yml
